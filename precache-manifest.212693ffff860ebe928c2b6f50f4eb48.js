841d67de01273a475b4525a4a836e13d2d27bbf2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-02 20:57:19 +0000

    Deploy oblador/react-native-vector-icons to github.com/oblador/react-native-vector-icons.git:gh-pages

---
+++ precache-manifest.212693ffff860ebe928c2b6f50f4eb48.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a14a30236e4cec3011c5b2612519dae2",
+    "url": "/react-native-vector-icons/index.html"
+  },
+  {
+    "revision": "c9726f6ca9209c398c7a",
+    "url": "/react-native-vector-icons/static/css/main.cc78ae33.chunk.css"
+  },
+  {
+    "revision": "959069378cb8392cc850",
+    "url": "/react-native-vector-icons/static/js/2.55ce9c30.chunk.js"
+  },
+  {
+    "revision": "c9726f6ca9209c398c7a",
+    "url": "/react-native-vector-icons/static/js/main.6470ca91.chunk.js"
+  },
+  {
+    "revision": "af1b966b18ffb7df2dc2",
+    "url": "/react-native-vector-icons/static/js/runtime-main.63b4219d.js"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/react-native-vector-icons/static/media/AntDesign.3a2ba315.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/react-native-vector-icons/static/media/Entypo.744ce600.ttf"
+  },
+  {
+    "revision": "140c53a7643ea949007aa9a282153849",
+    "url": "/react-native-vector-icons/static/media/EvilIcons.140c53a7.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/react-native-vector-icons/static/media/Feather.ca9ce9ff.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-native-vector-icons/static/media/FontAwesome.b06871f2.ttf"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/react-native-vector-icons/static/media/FontAwesome5_Brands.13685372.ttf"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/react-native-vector-icons/static/media/FontAwesome5_Regular.db78b935.ttf"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/react-native-vector-icons/static/media/FontAwesome5_Solid.1ab236ed.ttf"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "/react-native-vector-icons/static/media/Fontisto.b49ae8ab.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/react-native-vector-icons/static/media/Foundation.e20945d7.ttf"
+  },
+  {
+    "revision": "b3263095df30cb7db78c613e73f9499a",
+    "url": "/react-native-vector-icons/static/media/Ionicons.b3263095.ttf"
+  },
+  {
+    "revision": "6a2ddad1092a0a1c326b6d0e738e682b",
+    "url": "/react-native-vector-icons/static/media/MaterialCommunityIcons.6a2ddad1.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/react-native-vector-icons/static/media/MaterialIcons.a37b0c01.ttf"
+  },
+  {
+    "revision": "8e7f807ef943bff1f6d3c2c6e0f3769e",
+    "url": "/react-native-vector-icons/static/media/Octicons.8e7f807e.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/react-native-vector-icons/static/media/SimpleLineIcons.d2285965.ttf"
+  },
+  {
+    "revision": "5cdf883b18a5651a29a4d1ef276d2457",
+    "url": "/react-native-vector-icons/static/media/Zocial.5cdf883b.ttf"
+  }
+]);
